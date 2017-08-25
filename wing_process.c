9a56cb3a58d5354340fe608df16d44b795fa0cf8
xiaoan <huangxiaoan@yonglibao.com>
2017-08-25 21:04:05 +0800

    mac support

--- wing_process.c
+++ wing_process.c
@@ -397,6 +397,7 @@ ZEND_METHOD(wing_process, wait) {
         WUNTRACED 若子进程进入暂停状态，则马上返回，但子进程的结束状态不予以理会。
         WIFSTOPPED(status)宏确定返回值是否对应与一个暂停子进程。
 	    */
+	RETURN_LONG(childpid);
 
 	#endif
 }
