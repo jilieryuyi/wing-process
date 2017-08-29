747c359a01f7503b67116fefcbf45aa1ff16ba19
xiaoan <huangxiaoan@yonglibao.com>
2017-08-29 08:45:34 +0800

    dev

--- wing_api.c
+++ wing_api.c
@@ -11,7 +11,7 @@ zval *wing_zend_read_property(zend_class_entry *scope, zval *object, const char 
 
 
 /**
-* ?��?????php??????????????php???????? <?php ???
+* ?��?????php??????????????php???????? <?php ???
 *
 * @param char* file
 * @return BOOL
@@ -49,5 +49,36 @@ int wing_file_is_php(const char *file)
 	fclose(handle);
 
 	return 0;
+}
+
+int wing_write_cmdline(int process_id, char *cmdline)
+{
+    const char *tmp = wing_get_tmp_dir();
+    char path[MAX_PATH] = {0};
+    strcpy(path, tmp);
+    strcpy((char*)(path+strlen(tmp)), "/");
+    char _process_id[32] = {0};
+    sprintf(_process_id, "%d", process_id);
+    strcpy((char*)(path+strlen(tmp)+1), _process_id);
+
+    if (access(path, F_OK) != 0) {
+        if (0 != mkdir(path, 0777)) {
+            return 0;
+        }
+    }
+
+    strcpy((char*)(path+strlen(tmp)+1+strlen(_process_id)), "/cmdline");
+
+    FILE *handle = fopen((const char*)path, "w");
+    if (handle) {
+        if (1 != fwrite(cmdline, strlen(cmdline), 1, handle)) {
+            fclose(handle);
+            //写入失败
+            return 0;
+        }
+        fclose(handle);
+        return 1;
+    }
+    return 0;
 }
 
