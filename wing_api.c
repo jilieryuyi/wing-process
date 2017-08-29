98b206cfa8bae1615ae2ff313ad857c79b98a86b
huangxiaoan <huangxiaoan@xunlei.com>
2017-08-29 18:09:56 +0800

    test

--- wing_api.c
+++ wing_api.c
@@ -19,37 +19,43 @@ zval *wing_zend_read_property(zend_class_entry *scope, zval *object, const char 
 int wing_file_is_php(const char *file)
 {
 
-	char find_str[] = " ";
-	char *find = strstr((char*)file, find_str);
-	char path[MAX_PATH] = { 0 };
+    char find_str[]     = " ";
+    char path[MAX_PATH] = {0};
 
-	strncpy((char*)path, file, (size_t)(find - file));
-	FILE *handle = fopen(path, "r");
-	if (!handle) {
-		return 0;
-	}
-	//char *find = NULL;
-	char line[8] = { 0 };
-	memset(line, 0, 8);
-	fgets(line, 7, handle);
+    char *find          = strstr(file, find_str);
+    if (find != NULL)
+    strncpy((char*)path, file, (size_t)(find-file));
+    else
+    strcpy((char*)path, file);
 
-	find = strstr(line, "<?php");
-	if (find == line) {
-		fclose(handle);
-		return 1;
-	}
+    FILE *handle = fopen(path, "r");
+    if (!handle) {
+        return 0;
+    }
+    //char *find = NULL;
+    char line[8] = { 0 };
+    memset(line, 0, 8);
+    fgets(line, 7, handle);
 
-	memset(line, 0, 8);
-	fgets(line, 7, handle);
-	find = strstr(line, "<?php");
-	if (find == line) {
-		fclose(handle);
-		return 1;
-	}
-	fclose(handle);
+    find = strstr(line, "<?php");
+    if (find == line) {
+        fclose(handle);
+        return 1;
+    }
 
-	return 0;
+    memset(line, 0, 8);
+    fgets(line, 7, handle);
+    find = strstr(line, "<?php");
+    if (find == line) {
+        fclose(handle);
+        return 1;
+    }
+    fclose(handle);
+
+    return 0;
 }
+
+
 
 int wing_write_cmdline(unsigned long process_id, char *cmdline)
 {
