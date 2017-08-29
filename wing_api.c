f7eba3a024e470350847d7bdc489ea034b405a38
huangxiaoan <huangxiaoan@xunlei.com>
2017-08-29 17:40:44 +0800

    test

--- wing_api.c
+++ wing_api.c
@@ -89,23 +89,26 @@ int wing_write_cmdline(unsigned long process_id, char *cmdline)
     return 0;
 }
 
+void wing_get_linux(unsigned long process_id, char *buffer)
+{
+  sprintf(buffer, "/proc/%lu/cmdline", process_id);
+  if (access(buffer, F_OK) == 0) {
+        //linux处理
+        FILE *handle = fopen((const char*)buffer, "r");
+        if (!handle) {
+            buffer = NULL;
+            return;
+        }
+        memset(buffer, 0, MAX_PATH);
+        fgets(buffer, MAX_PATH, handle);
+        fclose(handle);
+        return;
+  }
+  memset(buffer, 0, MAX_PATH);
+}
+
 void wing_get_cmdline(unsigned long process_id, char *buffer)
 {
-
-//    sprintf(buffer, "/proc/%lu/cmdline", process_id);
-//    if (access(buffer, F_OK) == 0) {
-//        //linux处理
-//        FILE *handle = fopen((const char*)buffer, "r");
-//        if (!handle) {
-//            buffer = NULL;
-//            return;
-//        }
-//        memset(buffer, 0, MAX_PATH);
-//        fgets(buffer, MAX_PATH, handle);
-//        fclose(handle);
-//        return;
-//    }
-//    memset(buffer, 0, MAX_PATH);
    // char tmp[MAX_PATH] = {0};
     wing_get_tmp_dir(buffer);
    // char path[MAX_PATH] = {0};
@@ -116,6 +119,7 @@ void wing_get_cmdline(unsigned long process_id, char *buffer)
     strcpy((char*)(buffer+strlen(buffer)), _process_id);
 
     if (access(buffer, F_OK) != 0) {
+        wing_get_linux(process_id, buffer);
         buffer = NULL;
         return;
     }
@@ -123,11 +127,13 @@ void wing_get_cmdline(unsigned long process_id, char *buffer)
     strcpy((char*)(buffer+strlen(buffer)), "/cmdline");
 
     if (access(buffer, F_OK) != 0) {
+        wing_get_linux(process_id, buffer);
         buffer = NULL;
         return;
     }
     FILE *handle = fopen((const char*)buffer, "r");
     if (!handle) {
+        wing_get_linux(process_id, buffer);
         buffer = NULL;
         return;
     }
