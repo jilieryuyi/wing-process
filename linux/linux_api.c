d18382d2aeca9df4adecfd55b0625a8c95c787c2
huangxiaoan <huangxiaoan@xunlei.com>
2017-08-30 18:07:12 +0800

    get process memory

--- linux/linux_api.c
+++ linux/linux_api.c
@@ -176,37 +176,31 @@ int wing_get_process_id()
  */
 unsigned long get_memory(int process_id)
 {
- char file[MAX_PATH] = {0};
- sprintf(file, "/proc/%d/status", process_id);
- FILE *sp = fopen(file, "r");
- if (!sp) {
-    return 0;
- }
-    char sbuffer[256] = { 0 };
-    	char mem[32] = { 0 };
-    	char *cs = NULL;
-    	char *end = NULL;
-    	int count = 0;
-    	while (!feof(sp)) {
-    		memset(sbuffer, 0, 256);
-    		fgets(sbuffer, 256, sp);
-    		//printf("%s", sbuffer);
-    		if (strncmp(sbuffer, "VmSize:", 7) == 0) {
-    			//printf("发现VmSize\r\n");
-    			cs = (char*)(sbuffer + 6);
-    			end = (char*)(sbuffer + strlen(sbuffer));
-    			while (cs++[0] != '\n') {
-    				if (cs[0] >=48 && cs[0] <= 57) {
-    					mem[count++] = cs[0];
-    				}
-    			}
+    char file[MAX_PATH] = {0};
+    sprintf(file, "/proc/%d/status", process_id);
+    FILE *sp = fopen(file, "r");
+    if (!sp) {
+       return 0;
+    }
+    char sbuffer[32] = { 0 };
+    char mem[16]     = { 0 };
+    char *cs         = NULL;
+    int count       = 0;
 
-    			break;
-    		}
-    	}
-        fclose(sp);
-    	//printf("使用内存：%s\r\n", mem);
-    	return atoi((const char*)mem);
+    while (!feof(sp)) {
+        memset(sbuffer, 0, 32);
+        fgets(sbuffer, 32, sp);
+        if (strncmp(sbuffer, "VmSize:", 7) == 0) {
+            cs = (char*)(sbuffer + 6);
+            while (*cs++ != '\n' && *cs >= 48 && *cs <= 57) {
+                mem[count++] = *cs;
+            }
+            break;
+        }
+    }
+
+    fclose(sp);
+    return atoi((const char*)mem);
 }
 
 int wing_kill(int process_id)
