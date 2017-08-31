2767086a96d70cde574687665ab3b1a44cf78806
huangxiaoan <huangxiaoan@xunlei.com>
2017-08-31 13:36:23 +0800

    命令行解析支持

--- linux/linux_api.c
+++ linux/linux_api.c
@@ -207,7 +207,7 @@ unsigned long wing_create_process(const char *command, char* output_file)
             switch (ac) {
             case 0:
              printf("执行文件：%s\r\n", _args[0]);
-                if (execl(PHP_PATH, "php", _args[0], NULL) < 0) {
+                if (execl(PHP_PATH, "php", "/home/tools/wing-process/tests/1 2.php", NULL) < 0) {
                     exit(0);
                 }
                 break;
