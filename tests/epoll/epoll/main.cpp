56304edd692eea740cd0300de50e05b1a24a6443
xiaoan <huangxiaoan@yonglibao.com>
2017-10-23 15:29:31 +0800

    add kqueue demo

--- tests/epoll/epoll/main.cpp
+++ tests/epoll/epoll/main.cpp
@@ -34,11 +34,9 @@ const int kWriteEvent = 2;
 void setNonBlock(int fd)
 {
     int flags = fcntl(fd, F_GETFL, 0);
-    
-    exit_if(flags<0, "fcntl failed");
+    exit_if(flags < 0, "fcntl failed");
     
     int r = fcntl(fd, F_SETFL, flags | O_NONBLOCK);
-    
     exit_if(r<0, "fcntl failed");
 }
 
@@ -46,11 +44,13 @@ void updateEvents(int efd, int fd, int events, bool modify)
 {
     struct kevent ev[2];
     int n = 0;
+
     if (events & kReadEvent) {
         EV_SET(&ev[n++], fd, EVFILT_READ, EV_ADD|EV_ENABLE, 0, 0, (void*)(intptr_t)fd);
     } else if (modify) {
         EV_SET(&ev[n++], fd, EVFILT_READ, EV_DELETE, 0, 0, (void*)(intptr_t)fd);
     }
+
     if (events & kWriteEvent) {
         EV_SET(&ev[n++], fd, EVFILT_WRITE, EV_ADD|EV_ENABLE, 0, 0, (void*)(intptr_t)fd);
     } else if (modify){
@@ -62,49 +62,61 @@ void updateEvents(int efd, int fd, int events, bool modify)
     exit_if(r, "kevent failed ");
 }
 
-void handleAccept(int efd, int fd) {
+void handleAccept(int efd, int fd)
+{
     struct sockaddr_in raddr;
     socklen_t rsz = sizeof(raddr);
     int cfd = accept(fd,(struct sockaddr *)&raddr,&rsz);
     exit_if(cfd<0, "accept failed");
+    
     sockaddr_in peer, local;
     socklen_t alen = sizeof(peer);
     int r = getpeername(cfd, (sockaddr*)&peer, &alen);
-    exit_if(r<0, "getpeername failed");
+    exit_if(r < 0, "getpeername failed");
     printf("accept a connection from %s\n", inet_ntoa(raddr.sin_addr));
+    
     setNonBlock(cfd);
     updateEvents(efd, cfd, kReadEvent|kWriteEvent, false);
 }
 
-void handleRead(int efd, int fd) {
+void handleRead(int efd, int fd)
+{
     char buf[4096];
     int n = 0;
+    
     while ((n=::read(fd, buf, sizeof buf)) > 0) {
-        printf("read %d bytes\n", n);
+        printf("read %d bytes %s\n", n, buf);
         int r = ::write(fd, buf, n); //写出读取的数据
         //实际应用中，写出数据可能会返回EAGAIN，此时应当监听可写事件，当可写时再把数据写出
-        exit_if(r<=0, "write error");
+        exit_if(r <= 0, "write error");
     }
-    if (n<0 && (errno == EAGAIN || errno == EWOULDBLOCK))
+    
+    if (n < 0 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
         return;
+    }
     exit_if(n<0, "read error"); //实际应用中，n<0应当检查各类错误，如EINTR
     printf("fd %d closed\n", fd);
     close(fd);
 }
 
-void handleWrite(int efd, int fd) {
+void handleWrite(int efd, int fd)
+{
     //实际应用应当实现可写时写出数据，无数据可写才关闭可写事件
     updateEvents(efd, fd, kReadEvent, true);
 }
 
-void loop_once(int efd, int lfd, int waitms) {
+void loop_once(int efd, int lfd, int waitms)
+{
     struct timespec timeout;
-    timeout.tv_sec = waitms / 1000;
-    timeout.tv_nsec = (waitms % 1000) * 1000 * 1000;
     const int kMaxEvents = 20;
+
+    timeout.tv_sec  = waitms / 1000;
+    timeout.tv_nsec = (waitms % 1000) * 1000 * 1000;
+
     struct kevent activeEvs[kMaxEvents];
     int n = kevent(efd, NULL, 0, activeEvs, kMaxEvents, &timeout);
     printf("epoll_wait return %d\n", n);
+
     for (int i = 0; i < n; i ++) {
         int fd = (int)(intptr_t)activeEvs[i].udata;
         int events = activeEvs[i].filter;
@@ -117,6 +129,7 @@ void loop_once(int efd, int lfd, int waitms) {
         } else if (events == EVFILT_WRITE) {
             handleWrite(efd, fd);
         } else {
+            printf("unknown event %d\r\n", events);
             exit_if(1, "unknown event");
         }
     }
@@ -124,13 +137,11 @@ void loop_once(int efd, int lfd, int waitms) {
 
 int main()
 {
-    short port  = 9999;
+    short port  = 9998;
     int epollfd = kqueue();
-    
     exit_if(epollfd < 0, "epoll_create failed");
     
     int listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    
     exit_if(listenfd < 0, "socket failed");
     
     struct sockaddr_in addr;
@@ -141,19 +152,19 @@ int main()
     addr.sin_addr.s_addr = INADDR_ANY;
     
     int r = ::bind(listenfd,(struct sockaddr *)&addr, sizeof(struct sockaddr));
-    
     exit_if(r, "bind to 0.0.0.0:%d failed %d %s", port, errno, strerror(errno));
     
     r = listen(listenfd, 20);
-    
     exit_if(r, "listen failed %d %s", errno, strerror(errno));
     
     printf("fd %d listening at %d\n", listenfd, port);
     
     setNonBlock(listenfd);
     updateEvents(epollfd, listenfd, kReadEvent, false);
+
     for (;;) { //实际应用应当注册信号处理函数，退出时清理资源
         loop_once(epollfd, listenfd, 10000);
     }
+
     return 0;
 }
