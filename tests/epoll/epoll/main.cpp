6c7435fe1bdd57ecc7ad6ba3a1d2db492823aa69
xiaoan <huangxiaoan@yonglibao.com>
2017-10-23 21:30:09 +0800

    add kqueue demo

--- tests/epoll/epoll/main.cpp
+++ tests/epoll/epoll/main.cpp
@@ -6,6 +6,8 @@
 //  Copyright © 2017年 yuyi. All rights reserved.
 //
 //  kqueue demo
+//  @link http://blog.csdn.net/bytxl/article/details/17526351
+//  @link https://www.ibm.com/developerworks/cn/aix/library/1105_huangrg_kqueue/
 //
 
 #include <iostream>
@@ -30,6 +32,8 @@ if (r) {\
 const int kReadEvent  = 1;
 const int kWriteEvent = 2;
 
+char* popQueue();
+
 /**
  * 将socket设置为非阻塞
  * @params fd socket资源句柄
@@ -46,7 +50,7 @@ void setNonBlock(int fd)
 /**
  * 添加或删除read、write监听
  * @param efd kqueue返回值
- * @param fd listen的socket句柄
+ * @param fd socket句柄
  * @param events 操作类型
  * @param modify 是否修改
  */
@@ -68,14 +72,14 @@ void updateEvents(int efd, int fd, int events, bool modify)
      */
     //添加read事件
     if (events & kReadEvent) {
-        EV_SET(&ev[n++], fd, EVFILT_READ, EV_ADD|EV_ENABLE, 0, 0, (void*)(intptr_t)fd);
+        EV_SET(&ev[n++], fd, EVFILT_READ, EV_ADD|EV_ERROR|EV_ENABLE, 0, 0, (void*)(intptr_t)fd);
     } else if (modify) {
         EV_SET(&ev[n++], fd, EVFILT_READ, EV_DELETE, 0, 0, (void*)(intptr_t)fd);
     }
 
     //添加write事件
     if (events & kWriteEvent) {
-        EV_SET(&ev[n++], fd, EVFILT_WRITE, EV_ADD|EV_ENABLE, 0, 0, (void*)(intptr_t)fd);
+        EV_SET(&ev[n++], fd, EVFILT_WRITE, EV_ADD|EV_ERROR|EV_ENABLE, 0, 0, (void*)(intptr_t)fd);
     } else if (modify) {
         EV_SET(&ev[n++], fd, EVFILT_WRITE, EV_DELETE, 0, 0, (void*)(intptr_t)fd);
     }
@@ -91,36 +95,44 @@ void updateEvents(int efd, int fd, int events, bool modify)
  * accept接受连接
  * @param efd kqueue
  * @param fd listen的socket句柄
+ * @param nums 新连接的个数
  */
-void handleAccept(int efd, int fd)
+void handleAccept(int efd, int fd, long nums)
 {
     struct sockaddr_in raddr;
     socklen_t rsz = sizeof(raddr);
-    int cfd = accept(fd,(struct sockaddr *)&raddr,&rsz);
-    exit_if(cfd < 0, "accept failed");
-    
+    int cfd, i, r;
     sockaddr_in peer;//, local;
     socklen_t alen = sizeof(peer);
-    int r = getpeername(cfd, (sockaddr*)&peer, &alen);
-    exit_if(r < 0, "getpeername failed");
-    printf("accept a connection from %s:%d\n", inet_ntoa(raddr.sin_addr), raddr.sin_port);
     
-    setNonBlock(cfd);
-    updateEvents(efd, cfd, kReadEvent|kWriteEvent, false);
+    for (i = 0; i < nums; i++) {
+        memset(&raddr, 0, rsz);
+        cfd = accept(fd,(struct sockaddr *)&raddr, &rsz);
+        exit_if(cfd < 0, "accept failed");
+    
+        memset(&peer, 0, alen);
+        r = getpeername(cfd, (sockaddr*)&peer, &alen);
+        exit_if(r < 0, "getpeername failed");
+        printf("accept a connection from %s:%d\n", inet_ntoa(raddr.sin_addr), raddr.sin_port);
+    
+        setNonBlock(cfd);
+        updateEvents(efd, cfd, kReadEvent|kWriteEvent, false);
+    }
 }
 
 /**
  * 读事件回调
  * @param efd kqueue
  * @param fd socket句柄
+ * @param bytes 缓冲区可读字节数
  */
-void handleRead(int efd, int fd)
+void handleRead(int efd, int fd, long bytes)
 {
-    char buf[4096];
+    char buf[bytes];
     ssize_t n = 0;
     
-    while ((n = ::read(fd, buf, sizeof buf)) > 0) {
-        printf("read %zd bytes %s\n", n, buf);
+    while ((n = ::read(fd, buf, bytes)) > 0) {
+        printf("read %zd bytes:\n%s\n\n", n, buf);
         ssize_t r = ::write(fd, buf, n); //写出读取的数据
         //实际应用中，写出数据可能会返回EAGAIN，此时应当监听可写事件，当可写时再把数据写出
         exit_if(r <= 0, "write error");
@@ -141,8 +153,52 @@ void handleRead(int efd, int fd)
  */
 void handleWrite(int efd, int fd)
 {
+    char *send_data = popQueue();
     //实际应用应当实现可写时写出数据，无数据可写才关闭可写事件
-    updateEvents(efd, fd, kReadEvent, true);
+    if (send_data == NULL) {
+        //关闭写事件
+        updateEvents(efd, fd, kWriteEvent, true);
+        return;
+    }
+    
+    ssize_t size = send(fd, send_data, strlen(send_data), 0);
+    if (size == -1) {
+        //send error
+    }
+}
+
+/**
+ * 写入发送队列
+ * @param buf 需要发送的数据
+ */
+void endQueue(int efd, int fd, const char *buf)
+{
+    //启用写事件
+    updateEvents(efd, fd, kWriteEvent, false);
+}
+
+/**
+ * 弹出队列 
+ */
+char* popQueue()
+{
+    return NULL;
+}
+
+void sendData(int efd, int fd, const char* buf, size_t buf_size)
+{
+    ssize_t size = send(fd, buf, buf_size, 0);
+    
+    if (size == -1) {
+        endQueue(efd, fd, buf);
+        return;
+    }
+    
+    if (size < buf_size) {
+        char *_buf = (char*)(buf+size);
+        endQueue(efd, fd, _buf);
+        return;
+    }
 }
 
 /**
@@ -166,14 +222,22 @@ void loop(int efd, int lfd, int waitms)
 
     for (int i = 0; i < n; i ++) {
         int fd = (int)(intptr_t)activeEvs[i].udata;
+        long data = activeEvs[i].data;
+        
+        //处理出错的socket
+        if (activeEvs[i].flags & EV_ERROR) {
+            close((int)activeEvs[i].ident);
+            continue;
+        }
+        
         int events = activeEvs[i].filter;
         
         if (events == EVFILT_READ) {
             //如果触发的socket等于监听的socket，说明有新的连接
             if (fd == lfd) {
-                handleAccept(efd, fd);
+                handleAccept(efd, fd, data);
             } else {
-                handleRead(efd, fd);
+                handleRead(efd, fd, data);
             }
         } else if (events == EVFILT_WRITE) {
             handleWrite(efd, fd);
